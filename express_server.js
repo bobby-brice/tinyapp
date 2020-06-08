8655543a0c774c3530340c43e05cdb99a1eb5969
bobby-brice <bobby.brice@gmail.com>
2020-06-08 19:35:01 +0000

    added urls_index file, html skeleton and anchor tags-placeholder to the short url

--- express_server.js
+++ express_server.js
@@ -2,10 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set('view engine', "ejs");
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//routes
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -19,6 +28,9 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
+
+//PORT
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
